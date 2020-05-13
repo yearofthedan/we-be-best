f936036012e3e3a8384225843748aca7a242d3f4
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2020-05-13 21:05:34 +1000

    Add graphql-subscriptions lib for proving out websockets

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import server from './server';
 import express from 'express';
 import path from 'path';
+import * as http from 'http';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,7 +9,7 @@ const spaPath = process.env.SPA_PATH || path.join(__dirname, '../../spa/dist');
 
 app.use('/', express.static(spaPath));
 
-server.applyMiddleware({ app });
+server.applyMiddleware({app});
 
 const initialise = async () => {
   await app.listen({port});
@@ -17,3 +18,24 @@ const initialise = async () => {
 initialise().then(() => {
   console.log(`🚀 Server is running on localhost: ${port}`);
 });
+
+
+app.use('/', express.static(spaPath));
+
+server.applyMiddleware({app});
+
+const initialise = async () => {
+  await app.listen({port});
+  try {
+    const httpServer = http.createServer(app);
+
+    const ser = await server();
+    ser.applyMiddleware({app});
+    ser.installSubscriptionHandlers(httpServer);
+    await httpServer.listen({port});
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+initialise().then(() => {
