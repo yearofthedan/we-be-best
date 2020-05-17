40886058872407919e2d7ee61485359d4cbd7d92
Dan Mutton <YearOfTheDan@users.noreply.github.com>
2020-05-17 22:41:53 +1000

    eject from apollo-boost and wire up client for subscription support

--- spa/src/main.ts
+++ spa/src/main.ts
@@ -1,13 +1,43 @@
-import ApolloClient from 'apollo-boost';
-import VueApollo from 'vue-apollo';
-import Vue from 'vue';
+import Vue from 'vue'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import VueApollo from 'vue-apollo'
 import App from './App.vue';
 
+const HOST_NAME = window.location.host;
+
+const httpLink = new HttpLink({
+  uri: `https://${HOST_NAME}/graphql`,
+})
+
+const wsLink = new WebSocketLink({
+  uri: `ws://${HOST_NAME}/graphql`,
+  options: {
+    reconnect: true,
+  },
+})
 Vue.config.productionTip = false;
 
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+    return definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
+
 const apolloClient = new ApolloClient({
-  uri: '/graphql',
-});
+  link,
+  cache: new InMemoryCache(),
+  connectToDevTools: true,
+})
 
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient,
--- spa/src/shims-vue.d.ts
+++ spa/src/shims-vue.d.ts
@@ -2,9 +2,3 @@ declare module '*.vue' {
   import Vue from 'vue';
   export default Vue;
 }
-
-declare module '*.gql' {
-  import { DocumentNode } from 'graphql'
-  const content: DocumentNode
-  export default content
-}
